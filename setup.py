e83297d2d8207e2eba7adc08a8493e685bec3662
Antonio Cuni <anto.cuni@gmail.com>
2010-11-16 01:00:28 +0100

    write setup.py, kill outdated/duplicated docstring

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os.path
+from setuptools import setup, find_packages
+
+readme = os.path.join(os.path.dirname(__file__), 'README')
+long_description = open(readme).read()
+
+setup(
+    name='fancycompleter',
+    version='0.1',
+    author='Antonio Cuni',
+    author_email='anto.cuni@gmail.com',
+    py_modules=['fancycompleter'],
+    url='http://bitbucket.org/antocuni/fancycompleter',
+    license='BSD',
+    description='colorful TAB completion for Python prompt',
+    long_description=long_description,
+    keywords='rlcompleter prompt tab color completion',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Topic :: Utilities",
+        ],
+)
+
