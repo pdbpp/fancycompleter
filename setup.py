e1c79b8bac6aec611e6af9d9189846cabec90794
Daniel Hahler <git@thequod.de>
2019-05-27 23:54:02 +0200

    Switch to setupmeta, update maintainer and pyrepl dep (#3)

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
-import codecs
-import sys
-import os.path
 from setuptools import setup
-
-readme = os.path.join(os.path.dirname(__file__), 'README')
-long_description = codecs.open(readme, 'r', 'utf-8').read()
-
-install_requires = []
-if sys.platform == 'win32':
-    install_requires += ['pyreadline']
-elif sys.version_info < (3,):
-    install_requires += ["pyrepl>=0.8.2"]
 
 setup(
     name='fancycompleter',
-    use_scm_version=True,
+    setup_requires="setupmeta",
+    versioning="devcommit",
+    maintainer="Daniel Hahler",
+    url='https://github.com/pdbpp/fancycompleter',
     author='Antonio Cuni',
     author_email='anto.cuni@gmail.com',
     py_modules=['fancycompleter'],
-    url='http://bitbucket.org/antocuni/fancycompleter',
     license='BSD',
     description='colorful TAB completion for Python prompt',
-    long_description=long_description,
     keywords='rlcompleter prompt tab color completion',
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -30,11 +19,15 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Intended Audience :: Developers",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Topic :: Utilities",
         ],
-    setup_requires=['setuptools_scm'],
-    install_requires=install_requires,
+    install_requires=[
+        "pyrepl @ git+https://github.com/pdbpp/pyrepl@master#egg=pyrepl",
+        "pyreadline;platform_system=='Windows'",
+    ]
 )
