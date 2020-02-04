2134c2a3c181c5eecd3a1343c0fd8c04d9ff1a86
Daniel Hahler <git@thequod.de>
2020-02-04 06:23:03 +0100

    setup.py: Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Intended Audience :: Developers",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
