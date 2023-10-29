70b728f06199a1880f64e6b14ef6ddd3aa486920
bretello <bretello@distruzione.org>
2023-10-29 16:25:33 +0100

    format with black/isort

--- misc/fancycompleterrc.py
+++ misc/fancycompleterrc.py
@@ -1,5 +1,6 @@
 from fancycompleter import DefaultConfig
 
+
 class Config(DefaultConfig):
-    prefer_pyrepl = False # force fancycompleter to use the standard readline
-    use_colors = True     # you need a patched libreadline for this
+    prefer_pyrepl = False  # force fancycompleter to use the standard readline
+    use_colors = True  # you need a patched libreadline for this
